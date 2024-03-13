b522f8ed944c9f847582e80d8dab0a1d2226a778
Jakub Przybylski <jakubp02.10@gmail.com>
2024-03-13 12:54:50 +0100

    Final toutches.

--- TPO1_PJ_S24512/src/zad2/Main.java
+++ TPO1_PJ_S24512/src/zad2/Main.java
@@ -11,7 +11,7 @@ public class Main {
   public static void main(String[] args) {
     Service s = new Service("Poland");
     String weatherJson = s.getWeather("Warsaw");
-    Double rate1 = s.getRateFor("PLN");
+    Double rate1 = s.getRateFor("USD");
     Double rate2 = s.getNBPRate();
     // ...
     // część uruchamiająca GUI
